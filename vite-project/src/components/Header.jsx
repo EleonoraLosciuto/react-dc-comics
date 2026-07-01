c4eddd612c864ac6e39d2610287b054573a85a88
Eleonora Losciuto <eleonora.losciuto@gmail.com>
2026-07-01 23:28:36 +0200

    header elements without style

---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import './Header.css';
+
+function Header() {
+    return <header>
+        <img src="../../img/dc-logo.png" alt="DC LOGO" />
+        <nav>
+            <ul>
+                <li><a href="#">Characters</a></li>
+                <li className="active"><a href="#">Comics</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Shop</a></li>
+            </ul>
+        </nav>
+    </header>
+};
+
+export default Header;
+
---
+++ vite-project/src/main.jsx
@@ -0,0 +1,12 @@
+// automatically injected code from Vite - import of react + App
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+
+// automatically injected code from Vite - root to send components to index.html
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
