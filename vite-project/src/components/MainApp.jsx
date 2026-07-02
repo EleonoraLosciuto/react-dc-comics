1f4f49b0294251fbaf05bacce80517c44cd2900e
Eleonora Losciuto <eleonora.losciuto@gmail.com>
2026-07-02 19:00:28 +0200

    navmenu completed

--- vite-project/src/components/MainApp.jsx
+++ vite-project/src/components/MainApp.jsx
@@ -10,20 +10,20 @@ function MainApp() {
         <div className='nav-menu'>
             <ul>
                 <li>
-                    <img src="../img/buy-comics-digital-comics.png" alt="Digital Comics Logo" />
+                    <img src="img/buy-comics-digital-comics.png" alt="Digital Comics Logo" />
                     <a href="#">digital comics</a>
                 </li>
                 <li>
-                    <img src="" alt="" />
+                    <img src="img/buy-comics-merchandise.png" alt="Merchandise Logo" />
                     <a href="#">dc merchandise</a></li>
                 <li>
-                    <img src="" alt="" />
+                    <img src="img/buy-comics-subscriptions.png" alt="Subscription Logo" />
                     <a href="#">subscription</a></li>
                 <li>
-                    <img src="" alt="" />
+                    <img src="img/buy-comics-shop-locator.png" alt="Shop Locator Logo" />
                     <a href="#">comic shop locator</a></li>
                 <li>
-                    <img src="" alt="" />
+                    <img src="img/buy-dc-power-visa.svg" alt="DC Power Visa Logo" />
                     <a href="#">dc power visa</a></li>
             </ul>
         </div>
