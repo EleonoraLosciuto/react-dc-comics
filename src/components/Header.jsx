cd1758f3657d20de82e339bf870cd1aff7527a13
Eleonora Losciuto <eleonora.losciuto@gmail.com>
2026-07-11 17:23:41 +0200

    Render navbar links dynamically using array map

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,71 @@
 import './Header.css';
 
+const navBarLinks = [
+    {
+        id: 1,
+        item: "Characters",
+        active: false
+    },
+    {
+        id: 2,
+        item: "Comics",
+        active: true
+    },
+    {
+        id: 3,
+        item: "TV",
+        active: false
+    },
+    {
+        id: 4,
+        item: "Movies",
+        active: false
+    },
+    {
+        id: 5,
+        item: "Games",
+        active: false
+    },
+    {
+        id: 6,
+        item: "Collectibles",
+        active: false
+    },
+    {
+        id: 7,
+        item: "Videos",
+        active: false
+    },
+    {
+        id: 8,
+        item: "Fans",
+        active: false
+    },
+    {
+        id: 9,
+        item: "News",
+        active: false
+    },
+    {
+        id: 10,
+        item: "Shop",
+        active: false
+    }
+];
+
+
 function Header() {
     return <header className='container'>
         <img src="img/dc-logo.png" alt="DC LOGO" />
         <nav>
             <ul>
-                <li><a href="#">Characters</a></li>
-                <li className="active"><a href="#">Comics</a></li>
-                <li><a href="#">TV</a></li>
-                <li><a href="#">Movies</a></li>
-                <li><a href="#">Games</a></li>
-                <li><a href="#">Collectibles</a></li>
-                <li><a href="#">Videos</a></li>
-                <li><a href="#">Fans</a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">Shop</a></li>
+                {navBarLinks.map((link) => (
+                    <li key={link.id} className={link.active ? "active" : undefined}><a href='#'>{link.item}</a></li>
+                ))}
             </ul>
+
         </nav>
     </header>
 }
 
 export default Header;
-
